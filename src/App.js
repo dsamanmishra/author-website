862419ce62c306cfcae814434696c2fd1285fd83
D S Aman <dsaman@highape.com>
2018-06-20 20:18:06 +0530

    Desktop Version Completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import image1 from './images/pic1.jpg';
 import image2 from './images/pic2.jpg';
 import image3 from './images/pic3.jpg';
 import image4 from './images/pic4.jpg';
+import book1 from './images/book1.png';
+import book2 from './images/book2.jpg';
+import fb from './images/facebook.png';
+import twitter from './images/twitter.png';
+import insta from './images/instagram.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="col-sm-8 col-sm-offset-2 col-xs-12">
-        <Header />
+      <div className="col-sm-9 col-sm-offset-1 col-xs-12">
+	      <div className="col-sm-8 col-sm-offset-2 col-xs-12">
+	        <Header />
+	        <Menu />
+	        <About />
+	        <Footer />
+	      </div>
       </div>
     );
   }
@@ -20,14 +30,14 @@ class Header extends Component {
   render() {
     return (
       <div className="col-sm-12 red-thick-line">
-        <h1 className="website-title">Vaibhav Lall</h1>
+        <h1 className="website-title">Vaibhav<br />Lall</h1>
         <div ClassName="row">
           <div className="col-sm-9 fading-images">
             <img className="image2" src={image2} />
             <img className="image3" src={image3} />
             <img className="image4" src={image4} />
           </div>
-          <div className="col-sm-3">
+          <div className="col-sm-3 non-fading-images">
             <img className="image1" src={image1} />
           </div>
         </div>
@@ -37,17 +47,48 @@ class Header extends Component {
   }
 }
 
+
+class Menu extends Component {
+  render() {
+    return (
+		<div className="col-sm-12 menu-div">
+			<a href="" className="menu-desktop-items">Blog</a>
+			<a href="" className="menu-desktop-items">Books</a>
+			<a href="" className="menu-desktop-items">Contact Me</a>
+		</div>
+
+    );
+  }
+}
+
+class About extends Component {
+  render() {
+    return (
+    	<div className="col-sm-12 about-book-cover">
+		    <div className="col-sm-6 about-div">
+			    <h2 className="about-title">About Me</h2>
+			    <p className="about-text">Hailing from a small city, I have achieved a lot in my life. It was a great journey and after having done such a great work, I would now like to share the wisdom that I have achieved so far with my readers. Hope you guys will love it and have a nice time reading my books. Stay tuned for more updates guys!</p>
+		    </div>
+		    <div className="col-sm-6 book-cover-div">
+		    	<img className="book-cover1" src={book1} />
+		    	<img className="book-cover2" src={book2} />
+		    	<div className="col-sm-12 contact-more-details">
+		    		<p className="contact-details-text">Contact me for more details</p>
+		    	</div>
+		    </div>
+		</div>
+
+    );
+  }
+}
+
 class Footer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="col-sm-12 social-media-links-div">
+            <img className="social-media-links" src={fb} />
+            <img className="social-media-links" src={twitter} />
+            <img className="social-media-links" src={insta} />
       </div>
     );
   }
